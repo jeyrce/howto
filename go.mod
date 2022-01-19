3494d5c8521a6e0655b8f9354a98179a4f8c95de
jianxin.lu <jianxin.lu@woqutech.com>
2022-01-19 19:08:29 +0800

    fsnotify监听文件变化

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/lithammer/shortuuid v3.0.0+incompatible // indirect
@@ -14,6 +15,7 @@ require (
 	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/sony/sonyflake v1.0.0 // indirect
 	github.com/tidwall/gjson v1.12.0 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/sqlite v1.2.6 // indirect
---
+++ pkg/fswatch_test.go
@@ -0,0 +1,45 @@
+package pkg
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"testing"
+	"time"
+)
+
+func TestFsWatch(t *testing.T) {
+	file := "/tmp/fs.txt"
+	t.Log("test file watch")
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		t.Fatal(err)
+	}
+	watcher.Add(file)
+	defer watcher.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	reset := false
+	for {
+		if reset {
+			if err := watcher.Add(file); err != nil {
+				t.Errorf("failed to add file")
+				time.Sleep(5 * time.Second)
+				continue
+			}
+		}
+		select {
+		case e := <-watcher.Events:
+			switch {
+			case e.Op&fsnotify.Create == fsnotify.Create:
+				t.Log("new file")
+			case e.Op&fsnotify.Remove == fsnotify.Remove || e.Op&fsnotify.Rename == fsnotify.Rename:
+				t.Log("restart")
+				reset = true
+			case e.Op&fsnotify.Write == fsnotify.Write:
+				t.Log("file changed")
+			default:
+				t.Log(e.Op.String())
+			}
+		}
+	}
+}
