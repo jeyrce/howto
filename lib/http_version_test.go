0f932e7187a15f2c5daeb0d5d29e79101faf5dbf
jeyrce <jeyrce@gmail.com>
2022-03-10 22:36:41 +0800

    omitempty

--- lib/http_version_test.go
+++ lib/http_version_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+type server struct {
+	http.Handler
+	maxConn uint
+}
+
+func (s *server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if s.maxConn < 0 {
+		s.maxConn = 1
+	}
+	s.Handler.ServeHTTP(writer, request)
+}
+
 func TestHttpVersion(t *testing.T) {
-	http.NewServeMux()
+	_ = http.NewServeMux()
 }
--- lib/map_test.go
+++ lib/map_test.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"encoding/json"
 	"testing"
 )
 
@@ -16,4 +17,43 @@ func TestMapKey(t *testing.T) {
 		"z": "ddd",
 	}
 	t.Log(m["a"] == "true", m["z"] == "ddd", m["y"] == "ccc")
+}
+
+func TestMapNew(t *testing.T) {
+	var m = make(map[string]string)
+	m["x"] = "xx"
+	t.Log(m["x"])
+}
+
+type xx struct {
+	Name string `json:"name,omitempty" yaml:"name,omitempty"`
+	Age  uint   `json:"age,omitempty" yaml:"age,omitempty"`
+}
+
+type yy struct {
+	Name string `json:"name"`
+	Age  uint   `json:"age"`
+}
+
+func TestOmitempty(t *testing.T) {
+	var (
+		emptyX = xx{
+			Name: "",
+			Age:  0,
+		}
+		emptyY = yy{
+			Name: "",
+			Age:  0,
+		}
+	)
+	marshal, err := json.Marshal(emptyX)
+	if err != nil {
+		return
+	}
+	bytes, err := json.Marshal(emptyY)
+	if err != nil {
+		return
+	}
+	t.Log(string(marshal)) // {}
+	t.Log(string(bytes))   // {"name":"","age":0}
 }
