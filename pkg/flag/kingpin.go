77747a78343a9e9c3c22a9ac5d4b187360dac168
jeyrce <jeyrce@gmail.com>
2022-03-22 10:19:48 +0800

    test path prefix join

--- pkg/flag/kingpin.go
+++ pkg/flag/kingpin.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    kingpin "gopkg.in/alecthomas/kingpin.v2"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-    addr  = kingpin.Flag("listen.addr", "地址").Default(":9090").String()
-    check = kingpin.Command("check", "检查配置")
-    file  = check.Arg("filename", "文件名").String()
+	addr  = kingpin.Flag("listen.addr", "地址").Default(":9090").String()
+	check = kingpin.Command("check", "检查配置")
+	file  = check.Arg("filename", "文件名").String()
 )
 
 func init() {
-    kingpin.HelpFlag.Short('h')
-    kingpin.Version("0.1.0")
-    kingpin.Parse()
+	kingpin.HelpFlag.Short('h')
+	kingpin.Version("0.1.0").VersionFlag.Short('v')
+	kingpin.Parse()
 }
 
 func main() {
-    switch kingpin.Parse() {
-    case check.FullCommand():
-        fmt.Println("检查成功", *file)
-    }
+	switch kingpin.Parse() {
+	case check.FullCommand():
+		fmt.Println("检查成功", *file)
+	}
 }
