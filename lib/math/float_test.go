3c9118aa74ee99fbd0a970fcf1576233e7cd0d73
jeyrce <jeyrce@gmail.com>
2023-08-08 10:19:57 +0800

    feat: float

--- lib/math/float_test.go
+++ lib/math/float_test.go
@@ -1,11 +1,26 @@
-package lib
+package math
 
 import (
+	"fmt"
+	"github.com/gogf/gf/util/gconv"
 	"math"
+	"strconv"
 	"testing"
 )
 
 func TestFloat(t *testing.T) {
+	var (
+		nums = []string{
+			"1", "2", "3.1415", "5.67", "242412.45353453",
+		}
+	)
+	for _, n := range nums {
+		f := gconv.Convert(n, "float64")
+		fmt.Println(strconv.ParseFloat(fmt.Sprintf("%.2f", f), 64))
+	}
+}
+
+func TestFloatNan(t *testing.T) {
 	var (
 		count    = 0
 		pageSize = 0 // 当0作为分母时将会计算出一个 Nan
