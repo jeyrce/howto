4e9f9f0d4f12c13eae26de576d540658aa5052f8
jeyrce <jeyrce@gmail.com>
2025-01-16 15:58:31 +0800

    feat: daily

--- lib/context_test.go
+++ lib/context_test.go
@@ -1,24 +1,34 @@
 package lib
 
 import (
-    "context"
-    "testing"
-    "time"
+	"context"
+	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
-    ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*3)
-    defer cancelFunc()
-    go testTimeout(ctx, time.Second*2)
-    for {
-        select {
-        case <-ctx.Done():
-            t.Log(ctx.Err())
-        }
-    }
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancelFunc()
+	go testTimeout(ctx, time.Second*2)
+	for {
+		select {
+		case <-ctx.Done():
+			t.Log(ctx.Err())
+		}
+	}
 }
 
 func testTimeout(ctx context.Context, timeout time.Duration) {
-    time.Sleep(timeout)
-    ctx.Deadline()
+	time.Sleep(timeout)
+	ctx.Deadline()
+}
+
+func do(ctx context.Context) {
+	context.WithValue(ctx, "date", "2024-11-22")
+}
+
+func TestValueContext(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "author", "jeyrce")
+	do(ctx)
+	t.Log(ctx.Value("date"))
 }
---
+++ testdata/jinrishici.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+xToken=$(curl https://v2.jinrishici.com/token | jq '.data')
+
